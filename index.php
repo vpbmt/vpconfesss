c3a9ee28fd5ad5ec4cf25daa05e0c13b84a1706a
vpbmt <139615664+vpbmt@users.noreply.github.com>
2024-03-09 11:51:34 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php 
+$page_title="Home Page";
+include('includes/header.php'); 
+include('includes/navbar.php'); 
+?>
+    <div class="py-5">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12 text-center">
+                    <h2>VP Confessions</h4>
+                    <h4>Your Confessions will be not take your name,
+They are end-to-end encrypted
+we just need a login for security purpose
+but none of your data will be shared to anyone
+
+you can confess confidently
+only the for whom the confession is and what is the confession will be shared no other than that will be shared</h5>
+                </div>
+            </div>
+        </div>
+    </div>
+I
+<?php include('includes/footer.php'); ?>
